23055d42270727278817b7b42b06772316a37a1a
dddeok <crew0813@gmail.com>
2019-07-06 19:25:50 +0900

    #modify current time in Write View

--- app/src/main/java/com/example/emotiondiary/ViewUi/Write.java
+++ app/src/main/java/com/example/emotiondiary/ViewUi/Write.java
@@ -9,18 +9,31 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.emotiondiary.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Write extends Fragment {
     Button button;
     View view;
+    TextView currentDate;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle
             savedInstanceState) {
         view = inflater.inflate(R.layout.write, container, false);
+
+        long now = System.currentTimeMillis();
+        Date date = new Date(now);
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        String getTime = format.format(date);
+
+        currentDate = view.findViewById(R.id.current_date);
+        currentDate.setText(getTime);
 
         button = (Button)view.findViewById(R.id.addTextBtn);
         button.setOnClickListener(new View.OnClickListener() {
